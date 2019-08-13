b5a95515ca0bec49d29692ada1293b90d7e2fb9c
gzy123 <448950802@qq.com>
2019-08-13 19:05:22 +0800

    feat: builder travis ci

--- test/unit/webpack.base.test.js
+++ test/unit/webpack.base.test.js
@@ -2,10 +2,8 @@ const assert = require('assert')
 describe('webpack base test case', () => {
     const baseConfig = require('../../lib/webpack.base')
     console.log(baseConfig)
-    // 'E:/code/webpack-project/builder-webpack/test/smoke/template/src/index/index.js'
-    // 'E:/code/webpack-project/builder-webpack/test/smoke/template/src/search/index.js'
     it('entry', () => {
-        assert.equal(baseConfig.entry.index, 'E:/code/webpack-project/builder-webpack/test/smoke/template/src/index/index.js')
-        assert.equal(baseConfig.entry.search, 'E:/code/webpack-project/builder-webpack/test/smoke/template/src/search/index.js')
+        assert.equal(baseConfig.entry.index.indexof('/test/smoke/template/src/index/index.js') > -1)
+        assert.equal(baseConfig.entry.search.indexof('/test/smoke/template/src/search/index.js') > -1)
     })
 })
